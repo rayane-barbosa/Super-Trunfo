70dfb4145ae7f38f37c11facef1185cca1ab314f
Rayane Barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-09-23 21:39:01 +0000

    feature: cardLayout added

---
+++ super-trunfo-app/src/components/CardsLayout/cardLayout.tsx
@@ -0,0 +1,10 @@
+import  React from 'react';
+
+export default function CardsLayout() {
+    return (
+        <div className="flex flex-col items-center justify-between p-24">
+            <img src='https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png' alt='card-super-trunfo' />
+        </div>
+
+    );
+}
