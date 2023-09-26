7c2eb1674a3b8946a5e63f8bba62eba22271a60e
Rayane Barbosa <82448039+rayane-barbosa@users.noreply.github.com>
2023-09-26 11:44:11 +0000

    feature: ScoreUpdate component implementation

---
+++ super-trunfo-app/src/app/components/Score/ScoreUpdate.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React, { useState } from "react";
+import { IWinner } from "@/app/components/BoardGame/Board";
+
+interface IScoreUpdateProps {
+  score: number;
+}
+
+export const ScoreUpdate = ({ score }: IScoreUpdateProps) => {
+  const [playerScore, setPlayerScore] = useState(0);
+  const [botScore, setBotScore] = useState(0);
+
+  const handleUpdate = (winner: IWinner) => {
+    if (winner.player === true) {
+      setPlayerScore(playerScore + 1);
+    } else if (winner.bot === true) {
+      setBotScore(botScore + 1);
+    } else {
+      console.log("Tied");
+    }
+
+    return (
+      <>
+        {" "}
+        <h1 className="text-2xl font-bold text-center">
+          Player Score: {score} / Bot Score: {score}{" "}
+        </h1>
+      </>
+    );
+  };
+};
